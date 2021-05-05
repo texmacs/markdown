08bd1e7df826c27e0d09d92b9162d58988e6acaf
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2021-05-05 23:33:08 +0200

    Some macros for the TransferLab style

---
+++ packages/aai-tfl.ts
@@ -0,0 +1,26 @@
+<TeXmacs|1.99.18>
+
+<style|source>
+
+<\body>
+  <assign|marginal-note|<macro|hpos|vpos|body|<with|dummy1|<value|marginal-note-width>|dummy2|<value|marginal-note-sep>|<compound|<merge|marginal-|<arg|hpos>|-note>|<arg|vpos>|<very-small|<arg|body>>|<arg|hpos>>>>>
+
+  <assign|render-small-figure|<macro|type|name|fig|cap|<tabular*|<tformat|<cwith|1|-1|1|-1|cell-lsep|0spc>|<cwith|1|-1|1|-1|cell-rsep|0spc>|<cwith|2|2|1|1|cell-height|0.5fn>|<twith|table-valign|B>|<cwith|3|3|1|1|cell-hyphen|t>|<cwith|3|3|1|1|cell-hmode|exact>|<cwith|3|3|1|1|cell-width|0.35par>|<cwith|3|3|1|1|cell-bsep|1sep>|<cwith|3|3|1|1|cell-tsep|1sep>|<cwith|3|3|1|1|cell-rsep|0spc>|<twith|table-hmode|auto>|<table|<row|<cell|<arg|fig>>>|<row|<cell|>>|<row|<\cell>
+    <\html-div-class|caption>
+      <small|<\surround|<figure-name|<arg|name><figure-sep>><list-caption|<arg|type>|<arg|cap>>|>
+        <arg|cap>
+      </surround>>
+    </html-div-class>
+  </cell>>>>>>>
+
+  \;
+</body>
+
+<\initial>
+  <\collection>
+    <associate|global-author|Miguel de Benito Delgado>
+    <associate|global-subject|>
+    <associate|global-title|Macros for the TransferLab>
+    <associate|page-medium|paper>
+  </collection>
+</initial>
