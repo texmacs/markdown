0d2f3ac136f80937da7ad7dee8fe0c973be68f01
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2021-07-18 17:54:20 +0200

    frontmatter macro to get download name

--- packages/markdown/hugo.ts
+++ packages/markdown/hugo.ts
@@ -5,7 +5,9 @@
 <\body>
   <assign|hugo-front|<xmacro|args|<flag|frontmatter parameters|orange>>>
 
-  <assign|dict|<xmacro|kvs|todo: display keys and values here>>
+  <assign|dict|<xmacro|kvs|dict (to do: display keys and values)>>
+
+  <assign|pdf-name|<macro|<use-module|(markdown-utils)><extern|download-name>>>
 </body>
 
 <\initial>
